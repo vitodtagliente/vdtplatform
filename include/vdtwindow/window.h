0fbb4f5193646641c215a1a212a2e361b865594f
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-01-14 22:49:39 +0100

    project creation

---
+++ include/vdtwindow/window.h
@@ -0,0 +1,22 @@
+/// Copyright (c) Vito Domenico Tagliente
+
+#pragma once
+
+#include "color.h"
+#include "context.h"
+#include "command_buffer.h"
+#include "graphics_api.h"
+#include "graphics_buffer.h"
+#include "image.h"
+#include "material.h"
+#include "material_library.h"
+#include "mesh.h"
+#include "render_command.h"
+#include "renderable.h"
+#include "renderer.h"
+#include "renderer_2d.h"
+#include "renderer_3d.h"
+#include "shader.h"
+#include "shader_program.h"
+#include "texture.h"
+#include "texture_library.h"
